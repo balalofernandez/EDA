f2f6e8eef8a73f5bb87c0736ab8721b5b979d50f
balalofernandez <balalo.fernandez@gmail.com>
2021-10-14 10:46:09 +0200

    ArrayBinaryTree clase

--- TADBinaryTree/src/Tree/BinaryTree/ArrayBinaryTree.java
+++ TADBinaryTree/src/Tree/BinaryTree/ArrayBinaryTree.java
@@ -1,7 +1,10 @@
 
 package Tree.BinaryTree;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+
+import TreeExceptions.InvalidPositionException;
 import material.Position;
 
 /**
@@ -9,10 +12,48 @@ import material.Position;
  * @author mayte
  */
 public class ArrayBinaryTree<E> implements BinaryTree<E> {
+    private ArrayList<Node<E>> tree;
+
+    private  class Node<T> implements Position<T>{
+        private  T e;
+        private boolean valid;
+        private int n;
+
+        @Override
+        public T getElement() {
+            return e;
+        }
+
+        public boolean isValid() {
+            return valid;
+        }
+
+        public int getN() {
+            return n;
+        }
+
+        public void setElement(E e) {
+            this.e = e;
+        }
+
+        public void setValid(boolean valid) {
+            this.valid = valid;
+        }
+
+        public void setN(int n) {
+            this.n = n;
+        }
+    }
 
     @Override
     public Position<E> left(Position<E> v) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Node<E> n = new Node<>();
+        try {
+            n = checkPosition(v);
+        } catch (InvalidPositionException e) {
+            e.printStackTrace();
+        }
+        return tree.get(n.getN()*2 +1);
     }
 
     @Override
@@ -119,5 +160,7 @@ public class ArrayBinaryTree<E> implements BinaryTree<E> {
     public BinaryTree<String> subTree(Position<String> h) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+    private Node<E> checkPosition(Position<E> p)throws InvalidPositionException {
+        throw new UnsupportedOperationException();
+    }
 }
